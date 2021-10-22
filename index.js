752e6f1f38744206d1b6a12755502b64b2eb8fc1
Alvaro <aljajigo94@hotmail.com>
2021-10-22 00:13:01 -0500

    Carga Cambios con ScrollReveal

---
+++ index.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.menu-vertical',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px',
+
+})
+
+sr.reveal('.element-left',{
+    duration: 2000,
+    origin: 'right',
+    distance: '-200px',
+
+})
+
+sr.reveal('.element-right',{
+    duration: 2000,
+    origin: 'left',
+    distance: '-200px',
+
+})
